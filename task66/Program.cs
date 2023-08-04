a5a8033639db8e8373d06d036a53998ff06dda4c
Макаров Сергей Владимирович <s.makarov@farmaimpex.ru>
2023-08-04 16:56:04 +0400

    task66

---
+++ task66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int summa(int num1, int num2)
+{
+    if (num1==num2) 
+    {
+        return num1;
+    }
+    else 
+    {
+        return summa(num1,num2-1) + num2;
+    }
+}
+Console.WriteLine("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+if (first>second)
+{
+    Console.WriteLine(summa(second,first));
+}
+else
+{
+    Console.WriteLine(summa(first,second));
+}
+;
